07e25a8f2320980523fccd59bb6d2c61ce7c872e
weijunwei <weijunweiwangyaxin@163.com>
2020-03-20 23:09:07 +0800

    添加个性化配置

--- src/main/java/com/spirng_jpa/jpa_demo/JpaDemoApplication.java
+++ src/main/java/com/spirng_jpa/jpa_demo/JpaDemoApplication.java
@@ -8,6 +8,17 @@ public class JpaDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JpaDemoApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  jpa启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
     }
+
 
 }
