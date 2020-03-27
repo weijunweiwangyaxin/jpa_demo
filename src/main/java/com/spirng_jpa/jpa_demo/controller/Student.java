1ca585b8ed373a240c449e4b65713e732e7eef17
weijunwei <weijunweiwangyaxin@163.com>
2020-03-28 01:05:16 +0800

    添加了gitee

--- src/main/java/com/spirng_jpa/jpa_demo/controller/Student.java
+++ src/main/java/com/spirng_jpa/jpa_demo/controller/Student.java
@@ -5,6 +5,7 @@ public class Student {
 	public static void main(String[] args) {
 		System.out.println("commit");
 		System.out.println("commit to Gitee");
+		System.out.println("commit to Gitee");
 		
 	}
 }
